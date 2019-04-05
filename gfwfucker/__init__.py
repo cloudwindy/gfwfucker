33158bea3a025438352ae58a2f0382c02bb2e1c9
cloudwindy <49033775+cloudwindy@users.noreply.github.com>
2019-04-05 18:40:31 +0800

    Update

--- gfwfucker/__init__.py
+++ gfwfucker/__init__.py
@@ -10,16 +10,15 @@ __version__ = '1.0'
 
 __all__ = ['GFWFucker']
 
-# config part
-BACKLOG = 128
+# ----- config -----
+# usage: number of client handler threads
+POOL_SIZE = 128
+# usage: size of each message from remote server
+BUFFER_SIZE = 2048
 
-# protocol part
-
+# ----- protocol -----
 # usage: salt in password's md5
 SALT = b'FuckYouGFW'
-
-# usage: max packet length's length
-LEN  = 4
 
 # sender: client
 # usage: keep connection alive
@@ -42,19 +41,19 @@ CONNECT    = b'\x02'
 # sender: client
 # usage: send messages to a connected server
 # data: given connection ID and messages
-# response: none
+# response: SUCCESS or FAILURE
 SEND       = b'\x03'
 
-# sender: client
-# usage: get recieved messages from a connected server
+# sender: server
+# usage: send recieved messages from a connected server
 # data: given connection ID and messages
-# response: none
-RECV       = b'\x04'
+# response: SUCCESS or FAILURE
+FORWARD    = b'\x04'
 
 # sender: client or server
 # usage: disconnect a connected server
 # data: given connection ID
-# response: none
+# response: DISCONNECT
 DISCONNECT = b'\x05'
 
 # sender: client
@@ -69,22 +68,20 @@ QUIT       = b'\x06'
 SUCCESS    = b'\xfe'
 FAILURE    = b'\xff'
 
-# usage: break
-class BreakException(Exception):
-    pass
-
+# ----- code ------
 # usage: main instance
 class GFWFucker:
     def __init__(self, addr, port, password):
-        self.srv = socket()
-        self.srv.bind((addr, port))
+        srv = socket()
+        srv.bind((addr, port))
+        self.srv = srv
         md5_obj = md5(SALT)
         md5_obj.update(password.encode())
         self.password = md5_obj.digest()
     def fuck(self):
         # fuck when you want
         self.srv.listen()
-        pool = Pool(BACKLOG)
+        pool = Pool(POOL_SIZE)
         while True:
             cli = self.srv.accept()
             cli_sock = cli[0]
@@ -96,8 +93,8 @@ class GFWFucker:
 # usage: handle a client
 class ClientHandler:
     def __init__(self, cli, password):
-        self.srv = RemoteHandler()
         self.cli = cli
+        self.srv = RemoteHandler(self)
         self.password = password
     def __call__(self):
         try:
@@ -113,10 +110,10 @@ class ClientHandler:
                     self.remote_connect()
                 elif self.command == SEND:
                     self.remote_send()
-                elif self.command == RECV:
-                    self.remote_recv()
                 elif self.command == QUIT:
                     self.close()
+                else:
+                    self.send(FAILURE, b'Unknown command')
         except BreakException:
             pass
         except Exception as e:
@@ -144,20 +141,20 @@ class ClientHandler:
                 self.send(FAILURE, b'Not logged in')
     def remote_connect(self):
         addr = inet_ntoa(self.data[:4])
-        port = int2bytes(self.data[4:8])
+        port = bytes2int(self.data[4:8])
         try:
-            srv_id = self.srv.new(addr, port)
+            srv_id = self.srv.get_id()
+            self.srv.new(addr, port)
         except Exception as e:
             self.send(FAILURE, repr(e))
         else:
             self.send(SUCCESS, srv_id)
     def remote_send(self):
+        srv_id = bytes2int(self.data[:4])
         try:
-            self.srv.send(self.data[4:])
+            self.srv.send(srv_id, self.data[4:])
         except Exception as e:
             self.send(FAILURE, repr(e))
-    def remote_recv(self):
-        self.srv.recv()
     def send(self, command, data = ''):
         self.cli.send(command)
         self.cli.send(int2bytes(len(data)))
@@ -171,24 +168,34 @@ class ClientHandler:
 
 # usage: handle clients' connections with remote server
 class RemoteHandler:
-    def __init__(self):
+    def __init__(self, cli_handler):
+        self.cli = cli_handler
         self.srv_list = []
-    def new(self, addr, port):
-        self.srv_list += socket()
-        self.srv_list[self.get_id()].connect((addr, port))
     def get_id(self):
-        return len(self.srv_list) - 1
+        # warning: get_id() mustn't be called after new()
+        return len(self.srv_list)
+    def new(self, addr, port):
+        srv = socket()
+        srv.setblocking(False)
+        srv.connect((addr, port))
+        self.srv_list += srv
     def send(self, srv_id, msg):
-        return self.srv_list[srv_id].send(msg)
-    def recv(self, srv_id, len):
-        return self.srv_list[srv_id].recv(len)
+        self.srv_list[srv_id].send(msg)
+    def handle(self, srv_id):
+        msg = self.srv_list[srv_id].recv()
+        self.cli.send(FORWARD, msg)
     def close(self, srv_id):
         self.srv_list[srv_id].close()
     def close_all(self):
         for srv in self.srv_list:
             srv.close()
+
 # usage: change between int and bytes
 def int2bytes(num):
     return int.to_bytes(num, 4, 'big')
 def bytes2int(num):
     return int.from_bytes(num, 4, 'big')
+
+# usage: break
+class BreakException(Exception):
+    pass
